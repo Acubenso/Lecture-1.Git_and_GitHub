461184076735ea1136a1629f4187228c40d0c3c0
Acubenso <moskalvasyljsdev@gmail.com>
2021-08-29 14:27:38 +0300

    First commit

---
+++ Home_work/2/main.js
@@ -0,0 +1,46 @@
+ 
+ function arrayChallenge(arr){
+    if(arr.length > 1){
+        let middlePoint = Math.floor(arr.length/2);
+        let leftHalf = arr.slice(0, middlePoint);
+        let rightHalf = arr.slice(middlePoint);
+
+        arrayChallenge(leftHalf);
+        arrayChallenge(rightHalf);
+
+        let elLeft = 0;
+        let elRight = 0;
+        let elementOfNewArray = 0;
+
+        while(elLeft < leftHalf.length && elRight < rightHalf.length){
+            if(leftHalf[elLeft] < rightHalf[elRight]){
+                arr[elementOfNewArray]= leftHalf[elLeft];
+                elLeft++;
+            }
+            else{
+                arr[elementOfNewArray] = rightHalf[elRight];
+                elRight++;
+            }
+            elementOfNewArray++;
+        }
+        while(elLeft < leftHalf.length)
+        {
+            arr[elementOfNewArray] = leftHalf[elLeft];
+            elLeft++;
+            elementOfNewArray++;
+        }
+        while(elRight < rightHalf.length)
+        {
+            arr[elementOfNewArray] = rightHalf[elRight];
+            elRight++;
+            elementOfNewArray++;
+        }
+        if(arr[elementOfNewArray] ==== (arr.[elementOfNewArray-1] + 1)){   
+                console.log(arr);
+            }
+        else console.log(-1);    
+    }   
+    return arr;
+ }
+
+
