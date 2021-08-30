f18529f10a34ef5d1db5a03eecb255d5edc8a1a2
Acubenso <moskalvasyljsdev@gmail.com>
2021-08-30 18:43:44 +0300

    My commit

--- Home_work/2/main.js
+++ Home_work/2/main.js
@@ -1,5 +1,28 @@
- 
- function arrayChallenge(arr){
+function arrayChallenge(arr){
+
+    let leftSum = 0;
+    for(let i = 0; i < arr.length/2; i++){
+        leftSum += arr[i];
+    }
+
+    let rightSum = 0;
+    for(let j = arr.length/2; j < arr.length; j++){
+        rightSum += arr[j];
+    }
+
+    if(leftSum === rightSum){
+        console.log(arr)
+    }else {
+        console.log(-1);
+    }
+} 
+
+arrayChallenge([4, 2, 6, 2, 6, 4]); // Result = [4, 2, 6, 2, 6, 4] 
+arrayChallenge([4, 2, 6, 21, 61, 41]); // Result = -1 
+
+
+///////////////////////////////////////////////////////////////////////////
+ function MergeSort(arr){
     if(arr.length > 1){
         let middlePoint = Math.floor(arr.length/2);
         let leftHalf = arr.slice(0, middlePoint);
