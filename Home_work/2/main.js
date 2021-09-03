53440edfa6670822a40936991d02877fa1927c79
Acubenso <moskalvasyljsdev@gmail.com>
2021-09-03 09:19:52 +0300

    Third commit

--- Home_work/2/main.js
+++ Home_work/2/main.js
@@ -1,4 +1,92 @@
-    const input = document.querySelector("#inputArr");
+        const input = document.querySelector("#inputArr");
+        const button = document.querySelector("#buttonArr");
+        const resultEl = document.querySelector(".resultArr");
+
+        let arr, checkResult, facts, allCombinationArray;
+
+        button.addEventListener("click", () => {
+          arr = input.value.split("").map((el) => (el = parseInt(el)));
+          allCombinationArray = [];
+          checkResult = -1;
+          facts = [];
+
+          for (let i = 0; i < fact(arr.length); i++) {
+            checkAllCombination(i, permutation(i, arr.slice(0)).join(""));
+          }
+
+          for (let i = 0; i < allCombinationArray.length; i++) {
+            checkResult = arrayChallenge(allCombinationArray[i]);
+
+            if (checkResult !== -1) {
+              break;
+            }
+          }
+
+          resultEl.innerHTML = "Result: " + checkResult;
+        });
+
+        function fact(N) {
+          if (N == 0 || N == 1) return 1;
+
+          if (facts[N]) return facts[N];
+
+          facts[N] = N * fact(N - 1);
+
+          return facts[N];
+        }
+
+        function permutation(index, A) {
+          let n = A.length;
+          let i = index + 1;
+          let res = [];
+
+          for (let t = 1; t <= n; t++) {
+            let f = fact(n - t);
+            let k = Math.floor((i + f - 1) / f);
+            res.push(A.splice(k - 1, 1)[0]);
+            i -= (k - 1) * f;
+          }
+
+          if (A.length) res.push(A[0]);
+
+          return res;
+        }
+
+        function checkAllCombination() {
+          let res = Array.prototype.slice.call(arguments);
+          let item = res[1].split("").map((el) => {
+            return parseInt(el);
+          });
+          allCombinationArray.push(item);
+        }
+
+        function arrayChallenge(arr) {
+          let result;
+
+          let leftSum = 0;
+          for (let i = 0; i < arr.length / 2; i++) {
+            leftSum += arr[i];
+          }
+
+          let rightSum = 0;
+          for (let j = arr.length / 2; j < arr.length; j++) {
+            rightSum += arr[j];
+          }
+
+          if (leftSum === rightSum) {
+            result = arr;
+          } else {
+            result = -1;
+          }
+
+          return result;
+        }
+        
+
+
+
+
+    /*const input = document.querySelector("#inputArr");
     const button = document.querySelector("#buttonArr");
     const resultEl = document.querySelector(".resultArr");
 
@@ -27,7 +115,7 @@
             result = -1;
         }
     return result;    
-    } 
+    }*/ 
 
 ///////////////////////////////////////////////////////////////////////////
 /* function MergeSort(arr){
